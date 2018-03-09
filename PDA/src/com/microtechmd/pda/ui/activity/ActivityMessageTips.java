dc26e706711f0fc83f5dfaf47e16f44bd884a909
Administrator <guadan0711@126.com>
2018-03-09 16:17:08 +0800

    增加提示信息控制界面

---
+++ PDA/src/com/microtechmd/pda/ui/activity/ActivityMessageTips.java
@@ -0,0 +1,95 @@
+package com.microtechmd.pda.ui.activity;
+
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.microtechmd.pda.R;
+import com.microtechmd.pda.library.entity.DataList;
+import com.microtechmd.pda.library.entity.EntityMessage;
+import com.microtechmd.pda.library.entity.ParameterMonitor;
+import com.microtechmd.pda.library.entity.monitor.DateTime;
+import com.microtechmd.pda.library.entity.monitor.Event;
+import com.microtechmd.pda.library.entity.monitor.History;
+import com.microtechmd.pda.library.entity.monitor.Status;
+import com.microtechmd.pda.library.parameter.ParameterGlobal;
+import com.microtechmd.pda.library.utility.SPUtils;
+import com.microtechmd.pda.ui.widget.myswitchbutton.LukeSwitchButton;
+import com.microtechmd.pda.util.AndroidSystemInfoUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class ActivityMessageTips extends ActivityPDA {
+    private LukeSwitchButton hi_switchBtn, low_switchBtn, comm_switchBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_messagetips);
+        hi_switchBtn = (LukeSwitchButton) findViewById(R.id.hi_message_switch);
+        low_switchBtn = (LukeSwitchButton) findViewById(R.id.low_message_switch);
+        comm_switchBtn = (LukeSwitchButton) findViewById(R.id.comm_message_switch);
+
+        initSwitchBtn();
+    }
+
+    private void initSwitchBtn() {
+        boolean hi_messageFlag = (boolean) SPUtils.get(this, HIMESSAGETIPS, true);
+        boolean low_messageFlag = (boolean) SPUtils.get(this, LOMESSAGETIPS, true);
+        boolean comm_messageFlag = (boolean) SPUtils.get(this, COMMMESSAGETIPS, true);
+
+        setSwitchState(hi_messageFlag, hi_switchBtn);
+        setSwitchState(low_messageFlag, low_switchBtn);
+        setSwitchState(comm_messageFlag, comm_switchBtn);
+
+        setSwitchToggleChange(HIMESSAGETIPS, hi_switchBtn);
+        setSwitchToggleChange(LOMESSAGETIPS, low_switchBtn);
+        setSwitchToggleChange(COMMMESSAGETIPS, comm_switchBtn);
+    }
+
+
+    private void setSwitchState(boolean state, LukeSwitchButton switchButton) {
+        if (state) {
+            switchButton.toggleOn();
+        } else {
+            switchButton.toggleOff();
+        }
+    }
+
+    private void setSwitchToggleChange(final String key, LukeSwitchButton switchButton) {
+        //开关切换事件
+        switchButton.setOnToggleChanged(new LukeSwitchButton.OnToggleChanged() {
+            @Override
+            public void onToggle(boolean on) {
+                if (on) {
+                    SPUtils.put(ActivityMessageTips.this, key, true);
+//                    Toast.makeText(ActivityMessageTips.this, "打开提示信息", Toast.LENGTH_SHORT).show();
+                } else {
+                    SPUtils.put(ActivityMessageTips.this, key, false);
+//                    Toast.makeText(ActivityMessageTips.this, "关闭提示信息", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onClickView(View v) {
+        super.onClickView(v);
+
+        switch (v.getId()) {
+            case R.id.ibt_back:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
